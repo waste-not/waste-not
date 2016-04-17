91eaef6de4d0cbd129173f476c4aff8e880d893f
jessethach <jessethach@gmail.com>
2016-04-17 09:19:34 -0700

    stashing changes

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import inventoryReducer from './reducer_inventory';
+import authenticationReducer from './reducer_authentication';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   inventory: inventoryReducer,
+  authenticated: authenticationReducer,
   form: formReducer
 });
 

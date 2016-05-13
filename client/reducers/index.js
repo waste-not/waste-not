4820f2448bc1b4d62071a1e8aefc1c9fda6b4c30
Jesse Thach <jessethach@users.noreply.github.com>
2016-05-12 17:37:57 -0700

    Revert "Form validation"

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import inventoryReducer from './reducer_inventory';
 
 import authReducer from './reducer_authentication';
-import validateUserFieldsReducer from './reducer_validation';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   inventory: inventoryReducer,
-  validateFields: validateUserFieldsReducer,
   form: formReducer,
   auth: authReducer
 });

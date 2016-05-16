1b3f5024654015a0dc7039c8c34d7111b7e6b035
Jesse Thach <jessethach@users.noreply.github.com>
2016-05-16 11:34:39 -0700

    Revert "Revert "Form validation""

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/waste_not');
 
 const authRouter = require(__dirname + '/routes/auth_routes');
 const inventoryRouter = require(__dirname + '/routes/inventory_routes');
+const publicRouter = require(__dirname + '/routes/public_routes');
 
 app.use('/api', authRouter);
 app.use('/api', inventoryRouter);
+app.use('/api', publicRouter);
 app.use(express.static(__dirname + '/dist'));
 app.use('/data', express.static(__dirname + '/data'));
 
